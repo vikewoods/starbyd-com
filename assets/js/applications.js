550017f7b848fcf46700bb4a5647d692783ebfa0
Vik Ewoods <support@voroninstudio.eu>
2013-09-12 12:05:54 +0300

    Menu dirty version

--- assets/js/applications.js
+++ assets/js/applications.js
@@ -4,4 +4,19 @@
 
 $(document).ready(function(){
 
+	var $li = $("#catalog-menu");
+	var $menu = $("#catalog-sub-menu");
+
+	$li.hover(
+		function(){
+			$menu.fadeIn('slow', function() {
+				$li.addClass('active-li');
+			});
+		}
+	);
+
+	$menu.mouseleave(function(){
+		$(this).fadeOut('fast', function(){});
+		$li.removeClass('active-li');
+	});
 });
